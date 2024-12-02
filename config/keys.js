23b9c9f16e5d10550e80ff6e51e7c5d2aa259ee2
Mohan V <mohan.v@iinterchange.com>
2024-12-02 12:32:21 +0530

    Update a backend files

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017",
+    mongoURI: "mongodb+srv://MohanVadivel:MohanVadivel@jobportal.nsmfy.mongodb.net/?retryWrites=true&w=majority&appName=jobportal",
     secretOrKey: "randomString",
     orgmail: "vadivelmohan2@gmail.com",
 	orgpass: "rrau pyck rojj zmjc"
